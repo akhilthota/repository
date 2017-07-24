226e39be671b7022c3006fe8e1a25ad29101b862
akhilthota <akhilthota111@gmail.com>
2017-07-24 16:14:41 +0530

    first

---
+++ src/main/java/com/firstgroup/firstartifact/FirstartifactApplication.java
@@ -0,0 +1,12 @@
+package com.firstgroup.firstartifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstartifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstartifactApplication.class, args);
+	}
+}
---
+++ src/test/java/com/firstgroup/firstartifact/FirstartifactApplicationTests.java
@@ -0,0 +1,16 @@
+package com.firstgroup.firstartifact;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FirstartifactApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
