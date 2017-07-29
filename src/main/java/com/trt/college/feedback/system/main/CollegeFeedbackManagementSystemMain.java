896246dbf593f555ed8a579031624569c420df5a
sachin <sachin@Jarvis>
2017-07-29 23:03:57 +1000

    Modified class file name

--- src/main/java/com/trt/college/feedback/system/main/CollegeFeedbackManagementSystemMain.java
+++ src/main/java/com/trt/college/feedback/system/main/CollegeFeedbackManagementSystemMain.java
@@ -1,15 +1,12 @@
-package com.firstgroup.firstartifact;
+package com.trt.college.feedback.system.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FirstartifactApplication {
+public class CollegeFeedbackManagementSystemMain {
 	
 	public static void main(String[] input) {
-		SpringApplication.run(FirstartifactApplication.class, input);
-		System.out.println("hello");
-		FirstartifactApplication f =new FirstartifactApplication();		
-		
+		SpringApplication.run(CollegeFeedbackManagementSystemMain.class, input);
 	}
 }
