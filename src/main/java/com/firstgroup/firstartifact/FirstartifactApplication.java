394661cbae5f3c5686f037a44e32ce122ebef85e
sachin <sachin@Jarvis>
2017-07-29 22:36:33 +1000

    added one line in code

--- src/main/java/com/firstgroup/firstartifact/FirstartifactApplication.java
+++ src/main/java/com/firstgroup/firstartifact/FirstartifactApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FirstartifactApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(FirstartifactApplication.class, args);
+	
+	public static void main(String[] input) {
+		SpringApplication.run(FirstartifactApplication.class, input);
 		System.out.println("hello");
+		FirstartifactApplication f =new FirstartifactApplication();		
+		
 	}
 }
