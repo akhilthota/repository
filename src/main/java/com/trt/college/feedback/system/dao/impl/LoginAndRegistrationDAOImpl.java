1da731ca12bd2020a573f4fa7e00dbdb4e355de9
GIT-Sachin <sachinkumar9710@gmail.com>
2017-08-20 00:50:53 +1000

    added exception handling

--- src/main/java/com/trt/college/feedback/system/dao/impl/LoginAndRegistrationDAOImpl.java
+++ src/main/java/com/trt/college/feedback/system/dao/impl/LoginAndRegistrationDAOImpl.java
@@ -1,6 +1,8 @@
 package com.trt.college.feedback.system.dao.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -17,10 +19,17 @@ public class LoginAndRegistrationDAOImpl implements LoginAndRegistrationDAO{
 	
 	@Override
 	public LoginBO fetchUser(String userName) {
-		LoginBO loginBO=jdbcTemplate.queryForObject(FETCH_USER_QUERY, LoginBO.class);
-		/*LoginBO loginBO=new LoginBO();
-		loginBO.setUserName("Sachin");
-		loginBO.setPassword("sachin");*/
+		LoginBO loginBO=null;
+		try{
+			loginBO=jdbcTemplate.queryForObject(FETCH_USER_QUERY, LoginBO.class);
+		}catch (EmptyResultDataAccessException e) {
+			System.out.println("NO DATA FOUND IN DATABASE");
+		}catch(IncorrectResultSizeDataAccessException e){
+			System.out.println("MORE THAN ONE USER");
+		}catch (Exception e) {
+			System.out.println();
+		}
+		
 		return loginBO;
 	}
 
