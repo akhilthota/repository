bf5c44133def31cbcdcc777aae18f1918058f0ab
GIT-Sachin <sachinkumar9710@gmail.com>
2017-08-19 22:26:18 +1000

    updated register model object

--- src/main/java/com/trt/college/feedback/system/domain/RegistrationBO.java
+++ src/main/java/com/trt/college/feedback/system/domain/RegistrationBO.java
@@ -1,59 +1,150 @@
 package com.trt.college.feedback.system.domain;
 
+import java.util.Date;
+
 public class RegistrationBO {
 
-	 private String FullName;
-	  private String Password;
-	  private String User;
-	  private String StudentId;
-	  private String email;
-	  private String Branch;
-	  private int phone;
-	  private int Dob; 
-	  public String getFullName() {
-	  return FullName;
-	  }
-	  public void setFullname(String FullName) {
-	  this.FullName = FullName;
-	  }
-	  public String getPassword() {
-	  return Password;
-	  }
-	  public void setPassword(String Password) {
-	  this.Password =Password;
-	  }
-	 public String getUser(){
-	 return User;
-	 }
-	public void setUser(String User){
-	this.User =User;
-	}
-	 
-	  public String getEmail() {
-	  return email;
-	  }
-	  public void setEmail(String email) {
-	  this.email = email;
-	  }
-	  public String getBranch() {
-	  return Branch ;
-	  }
-	  public void setBranch(String Branch) {
-	  this.Branch =Branch ;
-	  }
-	  public int getPhone() {
-	  return phone;
-	  }
-	  public void setPhone(int phone) {
-	  this.phone = phone;
-	  }
-	  public int getDob(){
-	  
-	  return Dob;
-	  }
-	  public void setDob(int Dob ){
-	  this.Dob= Dob;
-	  }
+	private String fullName;
+	private String password;
+	private String user;
+	private String studentId;
+	private String email;
+	private String branch;
+	private int phone;
+	private Date dob;
+
+	public String getFullName() {
+		return fullName;
 	}
 
-	
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getBranch() {
+		return branch;
+	}
+
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+
+	public int getPhone() {
+		return phone;
+	}
+
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((branch == null) ? 0 : branch.hashCode());
+		result = prime * result + ((dob == null) ? 0 : dob.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((fullName == null) ? 0 : fullName.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + phone;
+		result = prime * result + ((studentId == null) ? 0 : studentId.hashCode());
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RegistrationBO other = (RegistrationBO) obj;
+		if (branch == null) {
+			if (other.branch != null)
+				return false;
+		} else if (!branch.equals(other.branch))
+			return false;
+		if (dob == null) {
+			if (other.dob != null)
+				return false;
+		} else if (!dob.equals(other.dob))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (fullName == null) {
+			if (other.fullName != null)
+				return false;
+		} else if (!fullName.equals(other.fullName))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (phone != other.phone)
+			return false;
+		if (studentId == null) {
+			if (other.studentId != null)
+				return false;
+		} else if (!studentId.equals(other.studentId))
+			return false;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		return true;
+
+	}
+
+	@Override
+	public String toString() {
+		return "RegistrationBO [fullName=" + fullName + ", password=" + password + ", user=" + user + ", studentId="
+				+ studentId + ", email=" + email + ", branch=" + branch + ", phone=" + phone + ", dob=" + dob + "]";
+	}
+}
